e378b457d81703f29199870cf749ea51a24ec49e
Emmett Grebe <emmettgrebe@gmail.com>
2026-08-14 09:57:14 -0400

    Collapsible component made.

---
+++ emmett-grebe-portfolio/src/Collapsible.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+interface CollapsibleProps {
+  title: string;
+  children: any;
+}
+
+/**
+ * Makes a collapsible component that goes up and down with a click.
+ *
+ * @param param0 Title string, and all the children inside the collapsible.
+ * @returns A collapsible component that can go up and down.
+ */
+export default function Collapsible({ title, children }: CollapsibleProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <div
+      style={{
+        borderRadius: "4px",
+        marginBottom: "10px",
+      }}
+    >
+      <button
+        className="display-5"
+        onClick={() => setIsOpen(!isOpen)}
+        style={{
+          width: "100%",
+          padding: "10px",
+          textAlign: "left",
+          background: "rgb(248 249 250 / 50%)",
+          border: "none",
+          cursor: "pointer",
+        }}
+      >
+        {title} {isOpen ? "^" : "..."}
+      </button>
+
+      <div
+        style={{
+          maxHeight: isOpen ? "500px" : "0px",
+          overflow: "hidden",
+          transition: "max-height 0.3s ease-in-out",
+          padding: isOpen ? "10px" : "0 10px",
+          background: "rgb(248 249 250 / 25%)",
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
