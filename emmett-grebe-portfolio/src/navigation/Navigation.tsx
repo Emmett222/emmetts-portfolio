5e19ce96fa6d7216acd6bf93239dfc8b23ebbbc2
Emmett Grebe <emmettgrebe@gmail.com>
2026-08-14 09:42:12 -0400

    Removed contacts from navigation.

--- emmett-grebe-portfolio/src/navigation/Navigation.tsx
+++ emmett-grebe-portfolio/src/navigation/Navigation.tsx
@@ -19,7 +19,6 @@ function Navigation() {
           <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About Me</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
             <Nav.Link href="#education">Education</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
             <Nav.Link href="#workhistory">Work History</Nav.Link>
