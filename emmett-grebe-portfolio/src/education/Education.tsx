64a3dc3e7193d5a4e087988a3d80efba36c46cf4
Emmett Grebe <emmettgrebe@gmail.com>
2026-08-15 21:15:27 -0400

    Changed to collapsibles.

--- emmett-grebe-portfolio/src/education/Education.tsx
+++ emmett-grebe-portfolio/src/education/Education.tsx
@@ -1,31 +1,51 @@
 import { Container } from "react-bootstrap";
+import Collapsible from "../components/collapsible/Collapsible";
 
 function Education() {
   return (
     <Container id="education" className="sections">
       <div className="vh-100 d-flex flex-column justify-content-center align-items-center text-center">
-        <h1 className="display-1 mt-5">Education</h1>
-        <h3 className="display-6 mt-5">
-          Computer Science BS with Honors minor at James Madison University
-          (August 2023 - May 2027)
-        </h3>
-        <p className="lead">
-          I am currently pursuing a Bachelor of Science in Computer Science with
-          an Honors minor at James Madison University. My Academic career is
-          rooted in applied software development. Worked solo to working in
-          Agile teams to build robust applications. I have gained a strong
-          command of object-oriented design and the complete software
-          development life cycle.
-        </p>
-        <h3 className="display-6 mt-5">AWS Cloud Practitioner</h3>
-        <p className="lead">
-          This certification validates my foundational expertise in cloud
-          infrastructure, security best practices, and distributed architectural
-          patterns. This portfolio is hosted using AWS Lightsail and AWS Route
-          53.
-        </p>
-        <h3 className="display-6 mt-5">AWS Solutions Architect Associate</h3>
-        <p className="lead">Currently preparing for the exam.</p>
+        <h1 className="display-1 mb-5">Education</h1>
+        <Collapsible
+          title={
+            "Computer Science BS with Honors minor at James Madison University(August 2023 - May 2027)"
+          }
+          children={
+            <>
+              <p className="lead">
+                I am currently pursuing a Bachelor of Science in Computer
+                Science with an Honors minor at James Madison University. My
+                Academic career is rooted in applied software development.
+                Worked solo to working in Agile teams to build robust
+                applications. I have gained a strong command of object-oriented
+                design and the complete software development life cycle.
+              </p>
+            </>
+          }
+        ></Collapsible>
+
+        <Collapsible
+          title={"AWS Cloud Practitioner"}
+          children={
+            <>
+              <p className="lead">
+                This certification validates my foundational expertise in cloud
+                infrastructure, security best practices, and distributed
+                architectural patterns. This portfolio is hosted using AWS
+                Lightsail and AWS Route 53.
+              </p>
+            </>
+          }
+        ></Collapsible>
+
+        <Collapsible
+          title={"AWS Solutions Architect Associate"}
+          children={
+            <>
+              <p className="lead">Currently preparing for the exam.</p>
+            </>
+          }
+        ></Collapsible>
       </div>
     </Container>
   );
