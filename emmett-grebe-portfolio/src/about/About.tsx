e1d6045e5b726c2d7e1d05b3430587326f5593b2
Emmett Grebe <emmettgrebe@gmail.com>
2026-09-12 20:26:00 -0400

    About styling.

--- emmett-grebe-portfolio/src/about/About.tsx
+++ emmett-grebe-portfolio/src/about/About.tsx
@@ -11,63 +11,63 @@ function About() {
           piece.
         </h2>
         <table className="table mt-5">
-  <tr>
-    <th scope="col">Frontend</th>
-    <th scope="col">Backend & Cloud</th>
-    <th scope="col">Mobile & Desktop</th>
-    <th scope="col">Database & Architecture</th>
-    <th scope="col">Developer Tooling & Systems</th>
-  </tr>
-  <tr>
-    <td>JavaScript</td>
-    <td>Java</td>
-    <td>Kotlin</td>
-    <td>SQL</td>
-    <td>Git</td>
-  </tr>
-  <tr>
-    <td>TypeScript</td>
-    <td>Python</td>
-    <td>Android Jetpack Media3</td>
-    <td>Android Room</td>
-    <td>Linux</td>
-  </tr>
-  <tr>
-    <td>React</td>
-    <td>Django</td>
-    <td>Java Swing</td>
-    <td>Relational Databases</td>
-    <td>npm</td>
-  </tr>
-  <tr>
-    <td>Svelte</td>
-    <td>AWS</td>
-    <td></td>
-    <td></td>
-    <td>Vite</td>
-  </tr>
-  <tr>
-    <td>HTML</td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td>Junit</td>
-  </tr>
-  <tr>
-    <td>CSS</td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td>Bootstrap</td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-</table>
+          <tr>
+            <th scope="col">Frontend</th>
+            <th scope="col">Backend & Cloud</th>
+            <th scope="col">Mobile & Desktop</th>
+            <th scope="col">Database & Architecture</th>
+            <th scope="col">Developer Tooling & Systems</th>
+          </tr>
+          <tr>
+            <td>JavaScript</td>
+            <td>Java</td>
+            <td>Kotlin</td>
+            <td>SQL</td>
+            <td>Git</td>
+          </tr>
+          <tr>
+            <td>TypeScript</td>
+            <td>Python</td>
+            <td>Android Jetpack Media3</td>
+            <td>Android Room</td>
+            <td>Linux</td>
+          </tr>
+          <tr>
+            <td>React</td>
+            <td>Django</td>
+            <td>Java Swing</td>
+            <td>Relational Databases</td>
+            <td>npm</td>
+          </tr>
+          <tr>
+            <td>Svelte</td>
+            <td>AWS</td>
+            <td></td>
+            <td></td>
+            <td>Vite</td>
+          </tr>
+          <tr>
+            <td>HTML</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Junit</td>
+          </tr>
+          <tr>
+            <td>CSS</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Bootstrap</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
       </div>
     </Container>
   );
