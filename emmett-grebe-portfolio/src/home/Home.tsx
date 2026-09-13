2be914323df7b45faa761f6d8f1dc5c092cb4a74
Emmett Grebe <emmettgrebe@gmail.com>
2026-09-12 20:58:24 -0400

    Home gradient.

--- emmett-grebe-portfolio/src/home/Home.tsx
+++ emmett-grebe-portfolio/src/home/Home.tsx
@@ -29,8 +29,8 @@ function Home() {
             id="pfp"
           />
         </div>
-        <div className="d-flex flex-column justify-content-center align-items-center text-center m-5">
-          <h1 className="display-1 ">Emmett Grebe</h1>
+        <div className="d-flex flex-column justify-content-center align-items-center text-center m-5" id="topContainer">
+          <h1 className="display-1 mt-5">Emmett Grebe</h1>
           <h2 className="display-5">
             Programmer who focuses on writing clean and reusable code.
           </h2>
